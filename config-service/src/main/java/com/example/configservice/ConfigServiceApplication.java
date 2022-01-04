6e134615c574df0b4224526b160540c818a657d6
ameernaqvi98 <96500479+ameernaqvi98@users.noreply.github.com>
2022-01-04 10:31:54 -0600

    Update ConfigServiceApplication.java

--- config-service/src/main/java/com/example/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/example/configservice/ConfigServiceApplication.java
@@ -2,8 +2,8 @@ package com.example.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
